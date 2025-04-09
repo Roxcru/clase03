62de6e2a8e830a6f6fa9c3842e53553c7f2c22e0
Cesar Flores <roxcru_xx@hotmail.com>
2025-04-08 19:19:38 -0500

    mejoras etl_prb

--- etl.py
+++ etl.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 df = pd.read_csv('input.csv')
 df['processed'] = df['value'] * 10
-df.to_csv('output4.csv', index=False)
+df.to_csv('output.csv', index=False)
 print("ETL completado.")
